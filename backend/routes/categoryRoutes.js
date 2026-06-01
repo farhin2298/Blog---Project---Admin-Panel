de051579caabd075d5775323f43b815be3c9444d
farhin2298 <farhin118@gmail.com>
2026-06-01 21:59:54 +0530

    Add files via upload

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const categoryController = require('../controllers/categoryController');
+const isAuthenticated = require('../middleware/isAuthenticated');
+
+// All category routes require authentication
+router.use(isAuthenticated);
+
+// Routes
+router.get('/', categoryController.getAllCategories);
+router.get('/:id', categoryController.getCategoryById);
+router.post('/', categoryController.createCategory);
+router.put('/:id', categoryController.updateCategory);
+router.delete('/:id', categoryController.deleteCategory);
+
+module.exports = router;
+
